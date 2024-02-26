df5bd78449e4ea78c3cd478ec99367557a04026e
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-02-26 11:03:16 +0700

    Solve miintask 12

--- mintask07.cpp
+++ mintask07.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+void in_place_merge_sort(vector<int> &array, int s, int e);
+
+
 void swap(vector<int> &array, int i, int j) {
   int temp = array[i];
   array[i] = array[j];
@@ -28,51 +31,98 @@ void merge(vector<int> &array, int s1, int e1, int s2, int e2, int sb) {
 }
 
 void merge_sort(vector<int> &array, int start, int end, int bstart) {
-  if (end - start <= 0) {
+  // cout << "merge sort s = " << start << " e = " << end << " b = " << bstart << "\n";
+  // for (int i = 0; i < array.size(); i++) {
+  //     cout << array[i] << " ";
+  //   }
+  //   cout << "\n";
+  //   int t;
+  //   cin >> t;
+  if (end - start == 0) {
+    swap(array, start, bstart);
     return;
   }
   int middle = (end + start) / 2;
-  merge_sort(array, start, middle, bstart);
-  merge_sort(array, middle + 1, end, bstart);
+  //merge_sort(array, start, middle, bstart);
+  //merge_sort(array, middle + 1, end, bstart);
+  in_place_merge_sort(array, start, middle);
+  in_place_merge_sort(array, middle + 1, end);
   merge(array, start, middle, middle + 1, end, bstart);
-  while (start <= end) {
-    swap(array, start++, bstart++);
-  }
+  // while (start <= end) {
+  //   swap(array, start++, bstart++);
+  // }
 }
 
-void sorting_insert(vector<int> &array, int i) {
-  for (; i < array.size() - 1 && array[i + 1] < array[i]; i++) {
-    swap(array, i, i + 1);
+void sorting_insert(vector<int> &array, int s, int e) {
+  for (; s < e && array[s + 1] < array[s]; s++) {
+    swap(array, s, s + 1);
   }
 }
-
+// 0 1 2 3 4 5 6 
+//  4 5 6 
+//  4 - (6 - 4+ 1) = 1
+// pp + (pp - p1s)
+// p - 1 - p + (e - p + 1) = -1 + e - p + 1 = e - p
+// pp + pp - p1s - pp = pp - p1s
+//
+int ii = 1;
 void in_place_merge_sort(vector<int> &array, int s, int e) {
+  int jj = ii;
+  // cout << "inplace: s= " << s << " e = " << e <<  " i= " << ii++ <<"\n";
+  // for (int i = 0; i < array.size(); i++) {
+  //     cout << array[i] << " ";
+  //   }
+  //   cout << "\n";
+  //   cout << "s = " << s << " e = " << e << "\n";
+  //   int t;
+  //   cin >> t;
+  if (e - s == 1) {
+    if (array[s] > array[e]) {
+      swap(array, s, e);
+    }
+    return;
+  }
+  if (e - s <= 0) return;
   int p = (s + e) / 2 + 1;
-  int pp = (p + 1) / 2;
+  int pp = (s + p + 1) / 2;
   int p1s = 2 * pp - p;
-  merge_sort(array, p, e, 0);
+  //in_place_merge_sort(array, p, e);
+  merge_sort(array, p - (e - p + 1), p - 1, p);
+  //cout << "sort to second" << "\n";
+  // cout.flush();
   while (pp - p1s > 0) {
-    merge_sort(array, p1s, pp - 1, pp);
+    // cout << "p = " << p <<  " pp = " << pp << " p1s = " << p1s << "\n";
+    merge_sort(array, pp, pp + (pp - p1s) - 1, p1s);
+    //merge_sort(array, p1s, pp - 1, pp);
+    //in_place_merge_sort(array, p1s, pp - 1);
     merge(array, p1s, pp - 1, p, e, pp);
     p = pp;
-    pp = (p + 1) / 2;
+    pp = (s + p + 1) / 2;
     p1s = 2 * pp - p;
-    for (int i = 0; i < array.size(); i++) {
-      cout << array[i] << " ";
-    }
-    cout << "\n";
-    cout << "p = " << p << " pp = " << pp << " p1s = " << p1s << "\n";
+    
   }
-  sorting_insert(array, 0);
+  sorting_insert(array, s, e);
+  // for (int i = 0; i < array.size(); i++) {
+  //     cout << array[i] << " ";
+  //   }
+  //   cout << "\n";
+  //   cout << "s = " << s << " e = " << e << " p = " << p << " pp = " << pp << " p1s = " << p1s << "\n";
+  //   cout << "in place end i=" << jj << "\n"; 
+  //   cin >> t;
+
 }
 
 int main() {
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-  }
+  // int n;
+  // cin >> n;
+  // vector<int> a(n);
+  // for (int i = 0; i < n; i++) {
+  //   cin >> a[i];
+  // }
+  // vector<int> a = {4, 9, 4, 1, 0, 7, -1, 2, 4, 2, 10, 3};
+  vector<int> a = {-74,48,-20,2,10,-84,-5,-9,11,-24,-91,2,-71,64,63,80,28,-30,-58,-11,-44,-87,-22,54,-74,-10,-55,-28,-46,29,10,50,-72,34,26,25,8,51,13,30,35,-8,50,65,-6,16,-2,21,-78,35,-13,14,23,-3,26,-90,86,25,-56,91,-13,92,-25,37,57,-20,-69,98,95,45,47,29,86,-28,73,-44,-46,65,-84,-96,-24,-12,72,-68,93,57,92,52,-45,-2,85,-63,56,55,12,-85,77,-39};
+
+  int n = a.size();
   in_place_merge_sort(a, 0, n - 1);
   for (int i = 0; i < n; i++) {
     cout << a[i] << " ";
---
+++ mintask12.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+pair<int, int> partitionLomuto(vector<int> &array, int start, int end) {
+    srand((unsigned) time(NULL));
+    int ind = start + (rand() % (end - start + 1));
+    int pivot = array[ind];
+    swap(array[start], array[ind]);
+    int l = start, h = start;
+    for (int c = start + 1; c <= end; c++) {
+      if (array[c] < pivot) {
+        int tmp = array[c];
+        array[c] = array[h + 1];
+        array[h + 1] = array[l];
+        array[l] = tmp;
+        l++; h++;
+      } else if (array[c] == pivot) {
+        swap(array[h + 1], array[c]);
+        h++;
+      }
+    }
+    return {l, h};
+}
+
+pair<int, int> partitionHoar(vector<int> &array, int start, int end) {
+    srand((unsigned) time(NULL));
+    int ind = start + (rand() % (end - start + 1));
+    int pivot = array[ind];
+    //swap(array[start], array[ind]);
+    int i = start, j = end, c = start;
+    while (c <= j) {
+      if (array[c] < pivot) {
+        swap(array[i++], array[c++]);
+      } else if (array[c] == pivot) {
+        c++;
+      } else {
+        swap(array[c], array[j--]);
+      }
+    }
+
+    return {i, j};
+}
+
+void quickSort(vector<int> &array, int start, int end) {
+  if (end - start <= 0) return;
+  pair<int, int> pivots = partitionLomuto(array, start, end);
+  quickSort(array, start, pivots.first - 1);
+  quickSort(array, pivots.second + 1, end);
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  quickSort(a, 0, n - 1);
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+  }
+  cout << "\n";
+}

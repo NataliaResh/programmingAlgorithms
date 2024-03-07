4b8ef80a745ff427222f7119252e78291860cb5e
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-03-07 08:57:06 +0700

    add tests to mintask 15

--- mintask15.cpp
+++ mintask15.cpp
@@ -1,7 +1,8 @@
 #include <algorithm>
 #include <cstddef>
 #include <cstdlib>
-#include<iostream>
+#include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -22,7 +23,8 @@ class Vector {
 public:
   Vector() : Vector(16) {}
   Vector(int capacity) : capacity_(capacity), data_(new int[capacity_]) {}
-
+  
+  int size() const {return size_; }
   void push_back(int elem) {
     if (size_ == capacity_) {
       capacity_ *= 2;
@@ -54,19 +56,26 @@ public:
   }
 };
 
-int main() {
-  int n;
-  cin >> n;
-  Vector *a = new Vector();
+void test(int n) {
+  Vector *v = new Vector(4);
   for (int i = 0; i < n; i++) {
-    int t;
-    cin >> t;
-    a->push_back(t);
+    v->push_back(i);
+  }
+  assert(v->size() == n);
+  assert(v->at(n - 1) == n - 1);
+  for (int i = 0; i < n / 2; i++) {
+    v->pop_back();
+  }
+  assert(v->size() == n - n / 2);
+  assert(v->at(n - n / 2 - 1) == n - n / 2 - 1);
+  delete v;
+}
 
-  }
-  for (int i = 0; i < n; i++) {
-    cout << a->at(i) << " ";
-  }
-  cout << "\n";
-  delete a;
+int main() {
+  test(10);
+  test(16);
+  test(17);
+  test(35);
+  test(60);
+  cout << "All tests passed\n";
 }
